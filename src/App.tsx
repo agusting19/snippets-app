a4166af19015e4348eef78cfced375158406ee66
Agustin Gomez <agusleongomez@gmail.com>
2023-05-08 20:39:29 -0300

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { SnippetEditor, SnippetForm, SnippetList } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <SnippetForm />
-      <SnippetList />
-      <SnippetEditor />
+    <div className="h-screen text-white grid grid-cols-12">
+      <div className="col-span-3 bg-zinc-900">
+        <SnippetForm />
+        <SnippetList />
+      </div>
+      <div className="col-span-9 bg-neutral-950 flex justify-center items-center">
+        <SnippetEditor />
+      </div>
     </div>
   );
 };
