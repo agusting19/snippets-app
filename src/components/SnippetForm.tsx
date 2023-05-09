b59ca88c6ac8449adec310591e0711f8e2a0afa9
Agustin Gomez <agusleongomez@gmail.com>
2023-05-08 23:34:32 -0300

    created snippetForm component

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { desktopDir } from "@tauri-apps/api/path";
+import { writeTextFile } from "@tauri-apps/api/fs";
+
 const SnippetForm = () => {
-  return <div>SnippetForm</div>;
+  const [snippetName, setSnippetName] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const desktopPath = await desktopDir();
+    await writeTextFile(
+      `${desktopPath}/Agu/Snippets/${snippetName}.json`,
+      "{}"
+    );
+    setSnippetName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Write a snippet"
+        value={snippetName}
+        className="bg-zinc-800 w-full border-none outline-none p-2"
+        onChange={(e) => setSnippetName(e.target.value)}
+      />
+    </form>
+  );
 };
 
 export default SnippetForm;
