a6a4af11ac297fe97b09b8095738d35633951cbc
Agustin Gomez <agusleongomez@gmail.com>
2023-05-08 23:57:44 -0300

    created SnippedList component

--- src/components/SnippetList.tsx
+++ src/components/SnippetList.tsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import { desktopDir } from "@tauri-apps/api/path";
+import { readDir } from "@tauri-apps/api/fs";
+import { useSnippetsStore } from "../store/snippetsStore";
+
 const SnippetList = () => {
-  return <div>SnippetList</div>;
+  const setSnippetsNames = useSnippetsStore((state) => state.setSnippetsNames);
+  const snippetsNames = useSnippetsStore((state) => state.snippetsNames);
+
+  useEffect(() => {
+    const getSnippets = async () => {
+      const desktopPath = await desktopDir();
+      const result = await readDir(`${desktopPath}/Agu/Snippets`);
+      const fileNames = result.map((file) => file.name!);
+      setSnippetsNames(fileNames);
+    };
+    getSnippets();
+  }, []);
+
+  return (
+    <div>
+      {snippetsNames.map((snippet) => (
+        <div key={snippet}>
+          <h1>{snippet}</h1>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SnippetList;
